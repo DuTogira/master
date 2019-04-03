166fe10daea9bb15728a0260c2f83ee7bdfbc6f3
annjas26 <ankitajaswal@csus.edu>
2019-04-02 20:14:30 -0700

    Ankita's info updated.

--- DUT-ogira's Project/src/Hello.java
+++ DUT-ogira's Project/src/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		System.out.println("219884523 - This line was added by Austin Smothers.");
 		System.out.println("218882626 - Sam Lee.");
 		System.out.println("219940527 - This line was added by Matthew Calderon.");
-		System.out.println("218981738 - This line was added by Ankita Jaswal.");
+		System.out.println("218981738 - Ankita Jaswal.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
