848a6bab2997539b7e27e6a1731bca8352d0ac39
samlee916 <slee5331@gmail.com>
2019-04-02 18:08:38 -0700

    Sam revised his part of the assignment.

--- DUT-ogira's Project/src/Hello.java
+++ DUT-ogira's Project/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("219884523 - This line was added by Austin Smothers.");
-		System.out.println("218882626 - Sam Lee.");
+		System.out.println("218882626 - This line was added by Sam Lee.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
