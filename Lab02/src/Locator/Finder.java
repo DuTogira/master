5088cf7d8145e3455c66eb11533ad612c1db9585
annjas26 <ankitajaswal@csus.edu>
2019-05-03 18:32:58 -0700

    Edited Finder

--- Lab02/src/Locator/Finder.java
+++ Lab02/src/Locator/Finder.java
@@ -1,5 +1,28 @@
 package Locator;
-
-public class Finder {
-
+/**
+ * 
+ * @author Ankita
+ *
+ */
+public class Finder
+{
+	private int ID;
+	private String location;
+	
+	//Finder constructed using Tag Object
+	public Finder(Tags A)
+	{
+		this.ID = A.getId();
+		this.location = A.getLocation(); 
+	}
+	//Returns ID
+	public int getID()
+	{
+		return ID;
+	}
+	//Returns Location
+	public String getLocation()
+	{
+		return location;
+	}
 }
